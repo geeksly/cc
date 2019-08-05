a90df3100b74a92c028e9e901fc79755b0db796c
geeksly <924097134@qq.com>
2019-08-05 19:02:48 +0800

    extract github config from code to properties

---
+++ src/main/java/club/sly/community/MFspringbootApplication.java
@@ -0,0 +1,13 @@
+package club.sly.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MFspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MFspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/sly/community/MFspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package club.sly.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MFspringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
